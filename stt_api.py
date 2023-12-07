8bbd109f87de6b18df85d0b68c1ba713351aeb4c
yu <146072630+yudam616@users.noreply.github.com>
2023-12-07 12:57:23 +0900

    add speech utility

---
+++ stt_api.py
@@ -0,0 +1,29 @@
+import urllib3
+import json
+import base64
+
+def recognize_audio(audio_file_path):
+    openApiURL = "http://aiopen.etri.re.kr:8000/WiseASR/Recognition"
+    accessKey = "your accessKey"
+    languageCode = "korean"
+
+    with open(audio_file_path, "rb") as file:
+        audioContents = base64.b64encode(file.read()).decode("utf8")
+
+    requestJson = {
+        "argument": {
+            "language_code": languageCode,
+            "audio": audioContents
+        }
+    }
+
+    http = urllib3.PoolManager()
+    response = http.request(
+        "POST",
+        openApiURL,
+        headers={"Content-Type": "application/json; charset=UTF-8", "Authorization": accessKey},
+        body=json.dumps(requestJson)
+    )
+    
+    result_text = json.loads(response.data.decode('utf-8'))['return_object']['recognized']
+    return result_text
